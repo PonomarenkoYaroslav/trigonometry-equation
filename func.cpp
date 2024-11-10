d8f6acfdb29be4dabf05a8c6a2c4d65a0d830b7c
PonomarenkoYaroslav <148447725+PonomarenkoYaroslav@users.noreply.github.com>
2024-11-10 20:53:47 +0100

    Hotfix: Implemented sum of first 3 terms

--- func.cpp
+++ func.cpp
@@ -1,5 +1,22 @@
 #include "func.h"
+#include <cmath>
 
-double TrigFunction::FuncA() {
-    return 0;
+double TrigFunction::FuncA(int n, double x) {
+    double result = 0;
+    double x = 0.5;
+    double n = 2;
+    for (int i = 0; i < n; i++) {
+        double term = (factorial(2 * i) / (pow(4, i) * pow(factorial(i), 2) * (2 * i + 1))) * pow(x, 2 * i + 1);
+        result += term;
+    }
+    return result;
+}
+
+double factorial(int num) {
+    if (num == 0 || num == 1) return 1;
+    double result = 1;
+    for (int i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
 }
